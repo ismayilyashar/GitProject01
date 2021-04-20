e0801e5c6164f62b8d271238bf6d8fbec2a07eb7
ISMAYIL MAMMADOV <ismayilyashar@gmail.com>
2021-04-20 14:06:35 -0400

    third commit

--- src/day_02_package_01/Day02Class01.java
+++ src/day_02_package_01/Day02Class01.java
@@ -10,6 +10,12 @@ public class Day02Class01 {
 		// We are LEARNING git add . , git commit -m "message", git push.
 		
 		// On the upper left corner change your branch either to main or branch in order to have access to edit your code on remote repository.
+	
+		// Here is my new commit after last git pull
+	
+	
+	
+	
 	}
 
 }
