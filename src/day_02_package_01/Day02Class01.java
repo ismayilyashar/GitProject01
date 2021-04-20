1267b0ddabaf168c7fc5b3f245fd66966eca5204
ISMAYIL MAMMADOV <ismayilyashar@gmail.com>
2021-04-20 13:40:20 -0400

    day 2 - commit 3

--- src/day_02_package_01/Day02Class01.java
+++ src/day_02_package_01/Day02Class01.java
@@ -4,6 +4,10 @@ public class Day02Class01 {
 	
 	public static void main(String[] args) {
 		
+		
+		//Hello everyone.
+		
+		// We are LEARNING git add . , git commit -m "message", git push.
 	}
 
 }
