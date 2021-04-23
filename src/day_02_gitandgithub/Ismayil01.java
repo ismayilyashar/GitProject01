53667ebdc6de053821298b8eed18003d4163eb91
m2m3w1 <ismayilyashar@gmail.com>
2021-04-22 20:53:27 -0400

    conflicting commit

--- src/day_02_gitandgithub/Ismayil01.java
+++ src/day_02_gitandgithub/Ismayil01.java
@@ -3,7 +3,8 @@ package day_02_gitandgithub;
 public class Ismayil01 {
 
 	public static void main(String[] args) {
-		System.out.println("Hello Everyone");
+		System.out.println("Hello Everyone");		
+		System.out.println("Here is another conflicting code on ismayil01");
 
 	}
 
